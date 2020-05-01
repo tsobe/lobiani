959632ebc51bd364a8eafd67e3f63e05f3f3f67c
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-05-01 23:28:06 +0200

    Added warehouse gradle module

---
+++ warehouse/src/main/kotlin/com/lobiani/warehouse/WarehouseApplication.kt
@@ -0,0 +1,11 @@
+package com.lobiani.warehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseApplication
+
+fun main(args: Array<String>) {
+	runApplication<WarehouseApplication>(*args)
+}
---
+++ warehouse/src/test/kotlin/com/lobiani/warehouse/WarehouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lobiani.warehouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
