446918145c065eaaef36261d310ae0421db83937
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-05-11 23:17:05 +0200

    Added another test endpoint

--- app/src/main/kotlin/com/lobiani/app/TestController.kt
+++ app/src/main/kotlin/com/lobiani/app/TestController.kt
@@ -3,11 +3,8 @@ package com.lobiani.app
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.lang.IllegalArgumentException
 
-/**
- * @author Beka Tsotsoria
- */
+
 @RestController
 class TestController {
 
@@ -20,4 +17,10 @@ class TestController {
     fun hello(@RequestParam(defaultValue = "John") name: String): String {
         return "Hello $name"
     }
+
+    @GetMapping
+    fun sum(@RequestParam a: Int, b: Int): Int {
+        return a + b
+    }
+
 }
