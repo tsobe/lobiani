f211f477b2a09826dbb89c5823f5d0fd79885cb4
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-05-11 22:41:10 +0200

    Added another test endpoint

--- app/src/main/kotlin/com/lobiani/app/TestController.kt
+++ app/src/main/kotlin/com/lobiani/app/TestController.kt
@@ -1,6 +1,7 @@
 package com.lobiani.app
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.lang.IllegalArgumentException
 
@@ -13,5 +14,10 @@ class TestController {
     @GetMapping("/error")
     fun error() {
         throw IllegalArgumentException("Boom");
+    }
+
+    @GetMapping
+    fun hello(@RequestParam(defaultValue = "John") name: String): String {
+        return "Hello $name"
     }
 }
