c279aa135034eca2fabd0ce6d3f5a9997cd6a971
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-07-04 16:09:46 +0200

    Changed vcp name

--- infra-config/terraform/test/main.tf
+++ infra-config/terraform/test/main.tf
@@ -4,8 +4,8 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_vpc" "test-fra1" {
-  name     = "test-fra1"
+resource "digitalocean_vpc" "test-fra2" {
+  name     = "test-fra2"
   region   = "fra1"
   ip_range = "10.115.0.0/20"
 }
