24447486e3b8d5ed3cea25c1e6162390a6a02a4f
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-07-15 22:25:49 +0200

    Configured Circle CI to include automatic test environment spin up

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -7,13 +7,15 @@ PROJECT_SLUG="bitbucket/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 function trigger_pipeline {
 	local check=$1
 	local build=$2
+	local run_e2e_tests=$3
 
     curl -s -u ${CIRCLE_API_USER_TOKEN}: \
 	   -H "Content-Type: application/json" \
 	   -d "{
 			  \"parameters\": {
 				  \"check\": $check,
-				  \"build\": $build
+				  \"build\": $build,
+				  \"run_e2e_tests\": $run_e2e_tests
 			  }
 		   }" \
 	   https://circleci.com/api/v2/project/$PROJECT_SLUG/pipeline
@@ -28,7 +30,7 @@ function collect_modified_dirs {
 	# First commit in a branch
 	if [[ ${last_successful_commit} == "null" ]]
 	then
-		commits="origin/master"
+		commits="origin/$CIRCLE_BRANCH"
 	else
 		commits="${CIRCLE_SHA1}..${last_successful_commit}"
 	fi
@@ -45,10 +47,15 @@ function was_modified {
 collect_modified_dirs
 
 code_dir="app"
+infra_config_dir="infra-config"
 if was_modified $code_dir
 then
 	echo "Triggering the build"
-  	trigger_pipeline false true
+  	trigger_pipeline false true false
+elif was_modified $infra_config_dir
+then
+	echo "Triggering e2e tests"
+	trigger_pipeline false false true
 else
-  	echo "No changes made to $code_dir, not triggering the build"
+  	echo "No changes made to $code_dir or $infra_config_dir, not triggering the build"
 fi
