f04b86325e202610ae55eab33359816abe214835
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-08-14 12:48:02 +0200

    Added actuator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ val e2eTestImplementation by configurations.getting {}
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.axonframework:axon-spring-boot-starter:$axonVersion")
 // Unfortunately Axon Server doesn't work with DevTools
