b47010072ea3e6b135d4adbffe4ec495cd91b3e9
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-08-28 22:55:22 +0200

    Removed static content from app module since it's handled by SPA

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,6 @@ val e2eTestImplementation by configurations.getting {}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.axonframework:axon-spring-boot-starter:$axonVersion")
