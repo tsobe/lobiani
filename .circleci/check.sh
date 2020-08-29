a154c05ad820e5d6444d7a706091d91cb1cff9c4
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-08-29 22:33:12 +0200

    Added build job for frontend

--- .circleci/check.sh
+++ .circleci/check.sh
@@ -8,15 +8,17 @@ PROJECT_SLUG="github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 
 function trigger_pipeline {
 	local check=$1
-	local build=$2
-	local test=$3
+	local build_backend=$2
+	local build_frontend=$3
+	local test=$4
 
     curl -s -u ${CIRCLE_API_USER_TOKEN}: \
 	   -H "Content-Type: application/json" \
 	   -d "{
 			  \"parameters\": {
 				  \"check\": $check,
-				  \"build\": $build,
+				  \"build_backend\": $build_backend,
+				  \"build_frontend\": $build_frontend,
 				  \"test\": $test
 			  }
 		   }" \
@@ -49,21 +51,28 @@ function was_modified {
 collect_modified_dirs
 
 backend_dir="app-backend"
+frontend_dir="app-frontend"
 infra_config_dir="infra-config"
 
 if was_modified $backend_dir
 then
-	echo "Triggering pipeline: \"build\""
-  	trigger_pipeline false true false
+	echo "Triggering pipeline: \"build_backend\""
+  	trigger_pipeline false true false false
+fi
+
+if was_modified $frontend_dir
+then
+	echo "Triggering pipeline: \"build_frontend\""
+  	trigger_pipeline false false true false
 fi
 
 if was_modified $infra_config_dir
 then
 	echo "Triggering pipeline: \"test\""
-	trigger_pipeline false false true
+	trigger_pipeline false false false true
 fi
 
-if ! was_modified $backend_dir && ! was_modified $infra_config_dir
+if ! was_modified $backend_dir && ! was_modified $frontend_dir && ! was_modified $infra_config_dir
 then
-  	echo "No changes made to $backend_dir or $infra_config_dir, not triggering any pipeline"
+  	echo "No changes made to $backend_dir, $frontend_dir or $infra_config_dir, not triggering any pipeline"
 fi
