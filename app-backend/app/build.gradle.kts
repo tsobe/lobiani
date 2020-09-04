dd1bcee205a3e86cf4ba352574f8b813f48920a8
Beka Tsotsoria <b.tsotsoria@gmail.com>
2020-09-04 23:08:09 +0200

    Extracted e2e tests into separate project

--- app-backend/app/build.gradle.kts
+++ app-backend/app/build.gradle.kts
@@ -7,15 +7,8 @@ plugins {
     kotlin("plugin.spring") version "1.3.71"
 }
 
-val gebVersion = "3.4"
-val seleniumVersion = "3.141.59"
-val e2eSourceSet = "e2eTest"
 val axonVersion = "4.2.1"
 val spockVersion = "2.0-M2-groovy-2.5"
-
-sourceSets.create(e2eSourceSet)
-
-val e2eTestImplementation by configurations.getting {}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -33,28 +26,4 @@ dependencies {
     testImplementation("org.spockframework:spock-spring:$spockVersion")
     testImplementation("org.testcontainers:testcontainers:1.14.3")
     testImplementation("org.testcontainers:spock:1.14.3")
-
-    e2eTestImplementation("org.spockframework:spock-core:$spockVersion")
-    e2eTestImplementation("org.gebish:geb-spock:$gebVersion") {
-        exclude(group = "org.codehaus.groovy")
-    }
-    e2eTestImplementation("org.seleniumhq.selenium:selenium-remote-driver:$seleniumVersion")
 }
-
-val defaultBaseUrl = "http://host.docker.internal:8080/"
-val defaultWebDriverUrl = "http://localhost:4444/wd/hub"
-
-tasks.register<Test>("e2eTestRemote") {
-    description = "Runs e2e tests against the remote WebDriver"
-    group = JavaBasePlugin.VERIFICATION_GROUP
-    outputs.upToDateWhen { false }
-    testClassesDirs = sourceSets[e2eSourceSet].output.classesDirs
-    classpath = sourceSets[e2eSourceSet].runtimeClasspath
-    shouldRunAfter("test")
-    systemProperty("geb.env", "remote")
-    systemProperty("geb.build.baseUrl", System.getProperty("geb.build.baseUrl", defaultBaseUrl))
-    systemProperty("remote.webdriver.url", System.getProperty("remote.webdriver.url", defaultWebDriverUrl))
-    systemProperty("remote.webdriver.acceptInsecure", System.getProperty("remote.webdriver.acceptInsecure"))
-    systemProperty("remote.webdriver.enableVideo", System.getProperty("remote.webdriver.enableVideo", "false"))
-}
-
