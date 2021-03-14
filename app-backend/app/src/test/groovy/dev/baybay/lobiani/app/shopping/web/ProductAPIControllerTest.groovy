59b6b4ea61e6bb1425f9d0cacc4b8ce64f279372
Beka Tsotsoria <b.tsotsoria@gmail.com>
2021-03-14 23:01:00 +0100

    Shopping API made publicly accessible

--- app-backend/app/src/test/groovy/dev/baybay/lobiani/app/shopping/web/ProductAPIControllerTest.groovy
+++ app-backend/app/src/test/groovy/dev/baybay/lobiani/app/shopping/web/ProductAPIControllerTest.groovy
@@ -3,14 +3,40 @@ package dev.baybay.lobiani.app.shopping.web
 import dev.baybay.lobiani.app.shopping.Price
 import dev.baybay.lobiani.app.shopping.PublishedProduct
 import dev.baybay.lobiani.app.shopping.query.QueryAllPublishedProducts
+import org.axonframework.commandhandling.CommandBus
 import org.axonframework.queryhandling.QueryGateway
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
 import java.util.concurrent.CompletableFuture
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductAPIController)
 class ProductAPIControllerTest extends Specification {
 
+    @Autowired
+    MockMvc mockMvc
+    @SpringBean
+    CommandBus commandBus = Mock()
+    @SpringBean
     QueryGateway queryGateway = Stub()
+
+    def "products endpoint should be publicly accessible"() {
+        given:
+        queryGateway.query(_ as QueryAllPublishedProducts, _) >> CompletableFuture.completedFuture([])
+
+        when:
+        def result = mockMvc.perform(get("/shopping/api/products"))
+
+        then:
+        result.andExpect(status().isOk())
+    }
 
     def "should return products when QueryBus returns results"() {
         given:
@@ -19,22 +45,25 @@ class ProductAPIControllerTest extends Specification {
         def title = "title"
         def description = "description"
         def stockLevel = 17
-        def price = new Price(new BigDecimal(17), Currency.getInstance("EUR"))
+        def priceVal = 17
+        def currency = "EUR"
+        def price = new Price(new BigDecimal(priceVal), Currency.getInstance(currency))
         def product = new PublishedProduct(id, slug, title, description, stockLevel, price)
 
         and:
         queryGateway.query(_ as QueryAllPublishedProducts, _) >> CompletableFuture.completedFuture([product])
 
-        and:
-        def controller = new ProductAPIController(queryGateway)
-
         when:
-        def products = controller.getProducts()
+        def result = mockMvc.perform(get("/shopping/api/products"))
 
         then:
-        products.size() == 1
-
-        and:
-        products[0] == product
+        result.andExpect(jsonPath("\$.length()").value(1))
+                .andExpect(jsonPath("\$[0].id").value(id))
+                .andExpect(jsonPath("\$[0].slug").value(slug))
+                .andExpect(jsonPath("\$[0].title").value(title))
+                .andExpect(jsonPath("\$[0].description").value(description))
+                .andExpect(jsonPath("\$[0].stockLevel").value(stockLevel))
+                .andExpect(jsonPath("\$[0].price.value").value(priceVal))
+                .andExpect(jsonPath("\$[0].price.currency").value(currency))
     }
 }
