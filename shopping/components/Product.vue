7ccc6c2d3eaf4104e3590c5ff331c4fd3802b7c1
Beka Tsotsoria <b.tsotsoria@gmail.com>
2021-03-20 23:09:36 +0100

    UI made responsive

--- shopping/components/Product.vue
+++ shopping/components/Product.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="font-sans bg-blue-50 shadow overflow-hidden rounded-lg border p-3">
-    <div class="flex items-center justify-between">
-      <span class="text-3xl font-mono font-light uppercase" data-title>{{ product.title }}</span>
-      <span class="font-bold uppercase" v-bind:class="stockSummary.class"
-            data-stock-summary>{{ stockSummary.text }}</span>
+    <div class="sm:flex items-center justify-between">
+      <div class="text-3xl font-mono font-light uppercase" data-title>{{ product.title }}</div>
+      <div class="font-bold uppercase" v-bind:class="stockSummary.class"
+            data-stock-summary>{{ stockSummary.text }}</div>
     </div>
-    <div class="flex items-center justify-between mt-2 pt-2 border-t border-gray">
-      <span data-description class="font-thin flex-auto border-r m-3">{{ product.description }}</span>
-      <p data-price class="flex-none">
+    <div class="sm:flex items-center justify-between mt-2 pt-2 border-t">
+      <div data-description class="font-thin flex-auto border-r m-3">{{ product.description }}</div>
+      <div data-price class="m-3 sm:m-0 sm:flex-none">
         <span>{{ product.price.value }}</span>
         <span class="text-green-600 font-bold">{{ currencySign }}</span>
-      </p>
+      </div>
     </div>
   </div>
 </template>
