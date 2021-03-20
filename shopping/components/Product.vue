804df66eae26e549fcf308c4771c8cefa5a46ed4
Beka Tsotsoria <b.tsotsoria@gmail.com>
2021-03-20 22:57:03 +0100

    Implemented conditional stock summary

--- shopping/components/Product.vue
+++ shopping/components/Product.vue
@@ -2,16 +2,14 @@
   <div class="font-sans bg-blue-50 shadow overflow-hidden rounded-lg border p-3">
     <div class="flex items-center justify-between">
       <span class="text-3xl font-mono font-light uppercase" data-title>{{ product.title }}</span>
-      <div>
-        <span class="font-light uppercase">Left in stock:</span>
-        <span data-stock-level>{{ product.stockLevel }}</span>
-      </div>
+      <span class="font-bold uppercase" v-bind:class="stockSummary.class"
+            data-stock-summary>{{ stockSummary.text }}</span>
     </div>
     <div class="flex items-center justify-between mt-2 pt-2 border-t border-gray">
       <span data-description class="font-thin flex-auto border-r m-3">{{ product.description }}</span>
       <p data-price class="flex-none">
         <span>{{ product.price.value }}</span>
-        <span class="text-green-600 font-bold">{{ product.price.currency }}</span>
+        <span class="text-green-600 font-bold">{{ currencySign }}</span>
       </p>
     </div>
   </div>
@@ -25,12 +23,28 @@ export default {
       required: true
     }
   },
-  data() {
-    return {
-      title: '',
-      description: '',
-      price: {},
-      stockLevel: 0
+  computed: {
+    stockSummary() {
+      const stockLevel = this.product.stockLevel
+      if (stockLevel === 0) {
+        return {
+          text: 'out of stock',
+          class: 'text-red-600'
+        }
+      }
+      if (stockLevel <= 10) {
+        return {
+          text: `only ${stockLevel} items left`,
+          class: 'text-yellow-600'
+        }
+      }
+      return {
+        text: 'in stock',
+        class: 'text-green-600'
+      }
+    },
+    currencySign() {
+      return '€'
     }
   }
 }
--- shopping/test/product.spec.js
+++ shopping/test/product.spec.js
@@ -1,28 +1,66 @@
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 import Product from '@/components/Product.vue'
 
 it('should display product information when mounted', () => {
-  const title = 'The Matrix'
-  const description = 'This is Matrix'
-  const price = '17'
-  const currency = 'EUR'
-  const stockLevel = '20'
-  const wrapper = mount(Product, {
+  const product = newProduct()
+
+  mountComponent(product)
+
+  expect(wrapper.find('[data-title]').text()).toBe(product.title)
+  expect(wrapper.find('[data-description]').text()).toBe(product.description)
+  expect(wrapper.find('[data-price]').text()).toBe(`${product.price.value} €`)
+})
+
+it('should display stock summary as OUT OF STOCK when stockLevel is 0', () => {
+  const stockLevel = 0
+  const product = newProduct(stockLevel)
+
+  mountComponent(product)
+
+  expect(stockSummaryWrapper.text()).toBe('out of stock')
+  expect(stockSummaryWrapper.classes('text-red-600')).toBe(true)
+})
+
+it('should display stock summary as IN STOCK when stockLevel is far greater than 0', () => {
+  const stockLevel = 30
+  const product = newProduct(stockLevel)
+
+  mountComponent(product)
+
+  expect(stockSummaryWrapper.text()).toBe('in stock')
+  expect(stockSummaryWrapper.classes('text-green-600')).toBe(true)
+})
+
+it('should display stock summary with warning when stockLevel is approaching 0', () => {
+  const stockLevel = 4
+  const product = newProduct(stockLevel)
+
+  mountComponent(product)
+
+  expect(stockSummaryWrapper.text()).toBe('only 4 items left')
+  expect(stockSummaryWrapper.classes('text-yellow-600')).toBe(true)
+})
+
+let wrapper
+let stockSummaryWrapper
+
+function mountComponent(product) {
+  wrapper = mount(Product, {
     propsData: {
-      product: {
-        title: title,
-        description: description,
-        price: {
-          value: price,
-          currency: currency
-        },
-        stockLevel: stockLevel
-      }
+      product: product
     }
   })
+  stockSummaryWrapper = wrapper.find('[data-stock-summary]')
+}
 
-  expect(wrapper.find('[data-title]').text()).toBe(title)
-  expect(wrapper.find('[data-description]').text()).toBe(description)
-  expect(wrapper.find('[data-price]').text()).toBe(`${price} ${currency}`)
-  expect(wrapper.find('[data-stock-level]').text()).toBe(stockLevel)
-})
+function newProduct(stockLevel = 20) {
+  return {
+    title: 'The Matrix',
+    description: 'This is Matrix',
+    price: {
+      value: '17',
+      currency: 'EUR'
+    },
+    stockLevel: stockLevel
+  }
+}
