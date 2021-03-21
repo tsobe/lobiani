b65c3d288d055060de623ac47adee0bc555f277b
Beka Tsotsoria <b.tsotsoria@gmail.com>
2021-03-21 20:03:42 +0100

    Introduced local firefox driver for E2E tests

--- e2e-tests/src/test/resources/GebConfig.groovy
+++ e2e-tests/src/test/resources/GebConfig.groovy
@@ -1,3 +1,4 @@
+import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 
@@ -16,6 +17,9 @@ environments {
             capabilities.setAcceptInsecureCerts(Boolean.getBoolean("remote.webdriver.acceptInsecure"))
             new RemoteWebDriver(URI.create(System.getProperty("remote.webdriver.url")).toURL(), capabilities)
         }
+    }
+    localFirefox {
+        driver = { new FirefoxDriver() }
     }
 }
 
